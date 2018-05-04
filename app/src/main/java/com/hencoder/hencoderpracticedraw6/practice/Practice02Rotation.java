8af324e94e51f5b72c3a65fdd56860160deb29a1
duanyuanjin <duanyuanjin@vlifepaper.com>
2018-05-04 17:54:04 +0800

    fist commit

---
+++ app/src/main/java/com/hencoder/hencoderpracticedraw6/practice/Practice02Rotation.java
@@ -0,0 +1,70 @@
+package com.hencoder.hencoderpracticedraw6.practice;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.hencoder.hencoderpracticedraw6.R;
+
+public class Practice02Rotation extends RelativeLayout {
+    Button animateBt;
+    ImageView imageView;
+    int i = 0;
+
+    public Practice02Rotation(Context context) {
+        super(context);
+    }
+
+    public Practice02Rotation(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice02Rotation(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        animateBt = (Button) findViewById(R.id.animateBt);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        animateBt.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                switch (i) {
+                    case 0:
+                        imageView.animate().rotation(180);
+                        break;
+                    case 1:
+                        imageView.animate().rotation(0);
+                        break;
+                    case 2:
+                        Log.d("dyj", "x = " + imageView.getX());
+                        imageView.animate().rotationX(180);
+                        break;
+                    case 3:
+                        imageView.animate().rotationX(0);
+                        break;
+                    case 4:
+                        Log.d("dyj", "Y = " + imageView.getY());
+                        imageView.animate().rotationY(180);
+                        break;
+                    case 5:
+                        imageView.animate().rotationY(0);
+                        break;
+                }
+                i++;
+                if (i > 5) {
+                    i = 0;
+                }
+            }
+        });
+    }
+}
